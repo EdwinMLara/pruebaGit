90adf7ddcf3911ffb212315ccad6646a1cf2982f
meli espinosa <meliespinosa490>
2023-08-02 15:28:46 -0600

    multipli

--- resta.js
+++ resta.js
@@ -6,3 +6,11 @@ function resta(num1, num2) {
   
   const resultado = resta(10, 5);
   console.log(resultado);
+
+function mul(num1, num2) {
+  return num1 * num2;
+}
+
+  const mult = mul(3, 5);
+  console.log(mult);
+
